8803b40fae4d0a8b4919f18d8e24fdd933f0b26e
keunyeong <91kyeong@gmail.com>
2022-04-04 18:45:12 +0900

    feat: 질문 말풍선 생성.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 const list = [
   {
-    image: "img/keunyeong.jpg",
+    image: "img/keunyeong.JPG",
     content: `안녕하세요. <span class="fetch">$</span>
     <span class="leedocs">Leedocs</span> 를 방문해 주셔서 감사합니다.`,
     isRead: true,
+    talker: "host",
   },
   {
-    image: "img/keunyeong.jpg",
+    image: "img/keunyeong.JPG",
     content: `<span class="fetch">$</span>
     <span class="leedocs">Leedocs</span> 는
     <span class="highlight">Keunyeong LEE</span>(이근영) 의 Documents를
     의미합니다.`,
     isRead: true,
+    talker: "host",
   },
   {
-    image: "img/keunyeong.jpg",
+    content: `<span class="fetch">$</span>
+    <span class="leedocs">Leedocs</span> 는 어떤 언어를 사용하여 만들었나요?`,
+    talker: "guest",
+  },
+  {
+    image: "img/keunyeong.JPG",
     content: `<span class="fetch">$</span>
     <span class="leedocs">Leedocs</span> 는
     <img
@@ -24,12 +31,14 @@ const list = [
     />
     <span class="highlight">Javascript</span>를 사용하여 제작되었습니다.`,
     isRead: true,
+    talker: "host",
   },
 ];
 
 document.querySelector("main").innerHTML = list
   .map((data, index) => {
-    return `<div class="talk_box">
+    if (data.talker === "host") {
+      return `<div class="talk_box">
     <div class="avatar">
       <img src=${data.image} alt="TALKER" />
     </div>
@@ -41,5 +50,15 @@ document.querySelector("main").innerHTML = list
       <div class="bookmark">읽음 - line ${index + 1}</div>
     </div>
   </div>`;
+    } else {
+      return `<div class="talk_box guest">
+    <div class="ballon">
+      <img class="tail_right" src="img/tail_right.svg" alt="TAIL" />
+      <p class="content">
+        ${data.content}
+      </p>
+    </div>
+  </div>`;
+    }
   })
   .join("");
