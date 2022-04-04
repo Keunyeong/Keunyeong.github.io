3b32ff67265adce1b65b90fbdfb06403940ba23e
keunyeong <91kyeong@gmail.com>
2022-04-04 22:45:07 +0900

    feat: 교육 수료 작성.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ const list = [
   {
     image: "img/keunyeong.JPG",
     content: `안녕하세요. <span class="fetch">$</span>
-    <span class="leedocs">Leedocs</span> 를 방문해 주셔서 감사합니다.`,
+    <span class="leedocs">Leedocs</span> 를 방문해 주셔서 감사합니다.<br/>
+    페이지 중앙을 <span class="highlight">클릭</span>하여 대화를 읽어주세요.<br/>
+    로고를 제외한 색이 다른 글자를 누르면 상세페이지가 열립니다.`,
     isRead: true,
     talker: "host",
   },
@@ -44,6 +46,50 @@ const list = [
   },
   {
     content: `개발을 위해 어떻게 학습하였나요?`,
+    talker: "guest",
+  },
+  {
+    image: "img/keunyeong.JPG",
+    content: `
+    21년 4월부터 5개월간 <a class="highlight2" href="https://velog.io/@leedocs/%EA%B5%AD%EB%B9%84%EA%B5%90%EC%9C%A1%EC%97%90%EC%9D%B4%EC%BD%98%EC%95%84%EC%B9%B4%EB%8D%B0%EB%AF%B8%ED%94%84%EB%A1%A0%ED%8A%B8%EC%95%A4%EB%93%9C-%EA%B0%9C%EB%B0%9C%EC%9E%90-%EC%96%91%EC%84%B1%EA%B3%BC%EC%A0%95" target="_blank">[에이콘아카데미]프론트엔드 개발자 양성과정</a> 을 수료하였습니다.
+    `,
+    isRead: true,
+    talker: "host",
+  },
+  {
+    image: "img/keunyeong.JPG",
+    content: `
+    이후 <a class="highlight2" href="https://leedocs.notion.site/React-8513815981e1474ca08f069f9ce01dc9" target="_blank">[스파르타코딩클럽]프론트엔드의 꽃 react</a> 온라인 강의를 통해 리엑트를 조금 알게 되었습니다.
+    `,
+    isRead: true,
+    talker: "host",
+  },
+  {
+    image: "img/keunyeong.JPG",
+    content: `
+    react 를 더 학습하고 다양한 직무분야의 팀원들과 기업과의 연계 프로젝트를 하기 위해 지원하여
+    `,
+    isRead: true,
+    talker: "host",
+  },
+  {
+    image: "img/keunyeong.JPG",
+    content: `
+    21년 11월부터 22년 3월까지<br/><a class="highlight2" href="https://velog.io/@leedocs/series/K-Digital-Training%ED%8C%A8%EC%8A%A4%ED%8A%B8%EC%BA%A0%ED%8D%BC%EC%8A%A4" target="_blank">[패스트캠퍼스]스타트업트랙 프론트엔드 개발자 양성 과정(블로그 기록)</a>을 🎖 <span class="highlight">우수 수료생</span><a class="highlight3" href="https://blog.naver.com/fastcampus/222651113796" target="_blank">(인터뷰)</a>으로 <br/> 수료하였습니다.
+    `,
+    isRead: true,
+    talker: "host",
+  },
+  {
+    image: "img/keunyeong.JPG",
+    content: `
+    코딩에 대한 열정으로 22년 2월에 열린 <br/> <a class="highlight3" href="https://velog.io/@leedocs/%EA%B0%9C%EB%B0%9C%EC%9D%BC%EA%B8%B0-%ED%95%B4%EC%BB%A4%ED%86%A4" target="_blank">[패스트캠퍼스 x 점핏] 해커톤</a> 에 참여하여 좋은 팀원들과 무수면 2박3일 코딩을 하여 <br/> 🏆<span class="highlight">최우수상</span> 을 수상하기도 하였습니다.
+    `,
+    isRead: true,
+    talker: "host",
+  },
+  {
+    content: `지금까지 개발한 프로젝트를 소개해주세요.`,
     talker: "guest",
   },
 ];
